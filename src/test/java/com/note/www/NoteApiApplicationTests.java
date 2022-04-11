27653c5a338e67fe494531dd204ca9ac869d6abe
강병훈 <kbh@ts-ei.com>
2022-04-11 14:11:29 +0900

    init

---
+++ src/test/java/com/note/www/NoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
